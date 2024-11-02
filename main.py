1b17a3fcf09a21dbc6594bda0300086718817ee4
Rio Sinnott <riorio@amilbox.org>
2024-11-02 00:21:14 -0700

    initial commit

---
+++ main.py
@@ -0,0 +1,51 @@
+import os
+import numpy as np
+from lightrag import LightRAG, QueryParam
+from lightrag.llm import *
+from lightrag.utils import EmbeddingFunc
+from PyPDF2 import PdfReader
+
+WORKING_DIR = "./photonDemo"
+
+if not os.path.exists(WORKING_DIR):
+    os.mkdir(WORKING_DIR)
+
+async def llm_model_func(
+    prompt, system_prompt=None, history_messages=[], **kwargs
+) -> str:
+    return await openai_complete_if_cache(
+        "L3-8B-Lunar-Stheno.Q4_K_M-HF",
+        prompt,
+        system_prompt=system_prompt,
+        history_messages=history_messages,
+        api_key=os.getenv("OPENAI_API_KEY"),
+        base_url="http://127.0.0.1:5000/v1",
+        **kwargs
+    )
+
+async def embedding_func(texts: list[str]) -> np.ndarray:
+    return await openai_embedding(
+        texts,
+        model="sentence-transformers/all-mpnet-base-v2",
+        api_key=os.getenv("OPENAI_API_KEY"),
+        base_url="http://127.0.0.1:5000/v1"
+    )
+
+rag = LightRAG(
+    working_dir=WORKING_DIR,
+    llm_model_func=llm_model_func,
+    embedding_func=EmbeddingFunc(
+        embedding_dim=768,
+        max_token_size=8192,
+        func=embedding_func
+    )
+)
+
+reader = PdfReader("book/Coaching_for_High_Performance.pdf")
+number_of_pages = len(reader.pages)
+page = reader.pages[0]
+text = page.extract_text()
+rag.insert(text)
+
+# Perform hybrid search
+print(rag.query("What are common challenges faced by professional coaches?", param=QueryParam(mode="hybrid")))
---
+++ openai_test.py
@@ -0,0 +1,31 @@
+import os
+from lightrag import LightRAG, QueryParam
+from lightrag.llm import gpt_4o_mini_complete, gpt_4o_complete
+from PyPDF2 import PdfReader
+
+#########
+# Uncomment the below two lines if running in a jupyter notebook to handle the async nature of rag.insert()
+# import nest_asyncio
+# nest_asyncio.apply()
+#########
+
+WORKING_DIR = "./chadDemo"
+
+
+if not os.path.exists(WORKING_DIR):
+    os.mkdir(WORKING_DIR)
+
+rag = LightRAG(
+    working_dir=WORKING_DIR,
+    llm_model_func=gpt_4o_mini_complete  # Use gpt_4o_mini_complete LLM model
+    # llm_model_func=gpt_4o_complete  # Optionally, use a stronger model
+)
+
+reader = PdfReader("book/Coaching_for_High_Performance.pdf")
+number_of_pages = len(reader.pages)
+page = reader.pages[0]
+text = page.extract_text()
+rag.insert(text)
+
+# Perform hybrid search
+print(rag.query("Can you define the role of a professional coach?", param=QueryParam(mode="hybrid")))
